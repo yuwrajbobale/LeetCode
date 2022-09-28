3767e1476b828625a8cbdf1d5a03b73a17f99aaa
Yuwraj Bobale <106270334+yuwrajbobale@users.noreply.github.com>
2022-09-28 23:36:44 +0530

    Time: 37 ms (11.62%), Space: 47.1 MB (6.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {    
-        if(s.length() != t.length()){
-            return false;
-        }
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
         
         HashMap<Character, Integer> map = new HashMap<>();
         
